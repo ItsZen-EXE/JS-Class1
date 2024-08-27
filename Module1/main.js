e587d7add5c3453c2d7a2639fa7ff9ef54588627
nkrehbielschool <17059333+ItsZen-EXE@users.noreply.github.com>
2024-08-27 11:11:26 -0500

    Moved files to independent folders

---
+++ Module1/main.js
@@ -0,0 +1,7 @@
+console.log("Hello, World!");
+console.log("Nathan Krehbiel");
+console.log("1999");
+console.log(1999);
+console.log("Nathan", "Krehbiel", "(1999)")
+console.log("Nathan" + "Krehbiel" + "(1999)")
+console.log("Nathan " + "Krehbiel " + "(1999)")
